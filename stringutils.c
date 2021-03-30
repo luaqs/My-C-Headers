37294fb3b3562205add73feda11af33bcb157a7e
JustACoder <58445942+CodingTheUniverse@users.noreply.github.com>
2021-03-30 14:01:10 -0400

    Create stringutils.c

---
+++ stringutils.c
@@ -0,0 +1,37 @@
+// I will Be Updating This Soon So Enjoy !
+/*
+    Sorry There Is Only 1 For Right Now !
+    +-----------------------+
+    | STRINGUTILS FUNCTIONS |
+    | Functions Included :
+        split_string << void | Used To Split A String With char *msg arg and char *delim |
+    ---------------------------------
+    -Works Fully ! 					-
+    -Include It In Your Projects !	-
+    - Created By Clever™#0948 		-
+    ---------------------------------
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Some Of This Split String Is Not Made Fully By Me I Had To Look At Stack Overflow !
+char **split_string(int *spaces_string, char *msg, char *delim)
+{
+    char **res;
+    char str[999];
+    strcpy(str, msg);
+    char *p = strtok(str, delim);
+    while (p)
+    {
+      res = realloc(res, sizeof (char*) * ++*spaces_string);
+      if (res == NULL)
+      {
+        exit (-1);
+      }
+      res[*spaces_string-1] = p;
+      p = strtok (NULL, delim);
+    }
+    res = realloc (res, sizeof (char*) * (*spaces_string+1));
+    res[*spaces_string] = 0;
+}
